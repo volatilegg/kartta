8b7345f5b083cc20657b5f74350e297bbaa6d3f1
Duc Do <duc.do@burstworld.io>
2024-09-17 17:17:30 +0300

    Init project

---
+++ kartta/karttaApp.swift
@@ -0,0 +1,17 @@
+//
+//  karttaApp.swift
+//  kartta
+//
+//  Created by Duc Do on 16.9.2024.
+//
+
+import SwiftUI
+
+@main
+struct karttaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ karttaTests/karttaTests.swift
@@ -0,0 +1,16 @@
+//
+//  karttaTests.swift
+//  karttaTests
+//
+//  Created by Duc Do on 16.9.2024.
+//
+
+import Testing
+
+struct karttaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
